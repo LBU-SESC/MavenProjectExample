6e535533c41e7541f47e5264d60a9accc15e9b33
dmullier <d.mullier@leedsbeckett.ac.uk>
2023-02-17 12:36:04 +0000

    Add files via upload

---
+++ EclipseMaven/src/main/java/uk/ac/leedsbeckett/mullier/EclipseMaven/App.java
@@ -0,0 +1,16 @@
+package uk.ac.leedsbeckett.mullier.EclipseMaven;
+import org.joda.time.LocalTime;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Maven Eclipse" );
+        LocalTime currentTime = new LocalTime();
+	    System.out.println("The current local time is: " + currentTime);
+
+    }
+}
